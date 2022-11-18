52c389de0e0b1bfe3d9089109063329b745a93c3
mika-alpha <yuukeikioku@gmail.com>
2022-11-18 18:10:09 -0500

    Fetch puzzle image from firebase storage

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 const Puzzle = () => {
+    
+    const [imgSrc, setImgSrc] = useState('/');
+
+    useEffect(() => {
+
+        const fetchImage = async () => {
+            const storage = getStorage();
+            await getDownloadURL(ref(storage, 'ps1-cut.png'))
+                .then((url) => setImgSrc(url))
+                    .catch(console.error);
+        }
+        
+        fetchImage();
+        
+    }, []);
+
 
     return (
         <div className="puzzle">
-            <img src="/" alt="puzzle"></img>
+            <img src={imgSrc} alt="puzzle"></img>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteSwitch from './RouteSwitch';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJctxM_JPuyebbUawEFmSWKzhzho__LGA",
+  authDomain: "where-is-nostalgia.firebaseapp.com",
+  projectId: "where-is-nostalgia",
+  storageBucket: "where-is-nostalgia.appspot.com",
+  messagingSenderId: "1059991258549",
+  appId: "1:1059991258549:web:76716b4cb9e16a8b4605fc",
+  measurementId: "G-ZVM344H38W"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
