513125b253a2ce649971000cfd4292d0719f1cd1
mika-alpha <yuukeikioku@gmail.com>
2022-11-18 14:59:49 -0500

    Refactor loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,15 +3,7 @@ import '../styles/loading.css';
 const Loading = () => {
 
     return (
-        <div class="loading">
-            <span>L</span>
-            <span>O</span>
-            <span>A</span>
-            <span>D</span>
-            <span>I</span>
-            <span>N</span>
-            <span>G</span>
-        </div>
+        <span className='loading'> LOADING </span>
     );
 }
 
