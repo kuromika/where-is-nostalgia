ac5bf5c915956285515b5db82036af9d80a6847e
mika-alpha <yuukeikioku@gmail.com>
2022-11-18 15:26:19 -0500

    Add components to Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,16 @@
+import Timer from "./Timer";
+import Characters from "./Characters"
+import Puzzle from "./Puzzle";
 
 const Game = () => {
     return (
-        <>
-        </>
+        <div className="game">
+            <div className="info-panel">
+                <Timer run={true}></Timer>
+                <Characters></Characters>
+            </div>
+            <Puzzle></Puzzle>
+        </div>
     )
 }
 
