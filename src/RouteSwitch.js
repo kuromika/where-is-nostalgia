9187011401495272900558a899c65c9def20e833
mika-alpha <yuukeikioku@gmail.com>
2022-11-17 20:34:19 -0500

    Create RouteSwitch

---
+++ src/RouteSwitch.js
@@ -0,0 +1,20 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom';
+import App from './App';
+
+const RouteSwitch = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App/>}>
+
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RouteSwitch;
