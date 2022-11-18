572fa64792ad1532339df38b2a220d34f29b7e1b
mika-alpha <yuukeikioku@gmail.com>
2022-11-17 20:55:09 -0500

    Create Loading component with css included

---
+++ src/components/Loading.js
@@ -0,0 +1,18 @@
+import '../styles/loading.css';
+
+const Loading = () => {
+
+    return (
+        <div class="loading">
+            <span>L</span>
+            <span>O</span>
+            <span>A</span>
+            <span>D</span>
+            <span>I</span>
+            <span>N</span>
+            <span>G</span>
+        </div>
+    );
+}
+
+export default Loading;
